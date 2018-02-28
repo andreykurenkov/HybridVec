3dd2a15bf3db11bfaa2ed96df79a29e096ae62f3
andreykurenkov <andreyk@stanford.edu>
2018-02-28 08:41:23 -0800

    "auto commit tracked files for new experiment: An Experiment on Feb-28-18@08:41:23"

--- definitions.py
+++ definitions.py
@@ -23,6 +23,7 @@ def get_a_definition(word):
     definitions = get_wordnet_definitions(word)
 
     if len(definitions) == 0:
+        print(get_glosbe_definitions(word))
         definitions = [g['text'] for g in get_glosbe_definitions(word)]
 
     if len(definitions) != 0:
