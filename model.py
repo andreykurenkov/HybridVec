1201f0b129bf39e5656fde430a9962bfc8e0ca73
andreykurenkov <andreyk@stanford.edu>
2018-02-28 11:53:37 -0800

    Correct implementation of packing

--- model.py
+++ model.py
@@ -16,8 +16,10 @@ class Def2VecModel(nn.Module):
                hidden_size=128, 
                embed_size=300, 
                num_layers=2,
-               use_cuda=False):
+               use_cuda=False,
+               use_packing=True):
     super(Def2VecModel, self).__init__()
+    self.use_packing = use_packing
     self.use_cuda = use_cuda
     self.vocab_size = len(vocab.stoi)
     self.embeddings = nn.Embedding(self.vocab_size, embed_size, padding_idx=0)
@@ -33,12 +35,14 @@ class Def2VecModel(nn.Module):
     inputs = Variable(inputs)
     batch_size, input_size = inputs.shape
     embed = self.embeddings(inputs.view(-1, input_size)).view(batch_size, input_size, -1)
-    embed = nn.utils.rnn.pack_padded_sequence(embed, lengths, batch_first=True)
+    if self.use_packing:
+      embed = nn.utils.rnn.pack_padded_sequence(embed, lengths, batch_first=True)
     h0 = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size))
     if self.use_cuda:
       h0 = h0.cuda()
     gru_outputs, _ = self.gru(embed, h0)
-    unpacked, unpacked_len = torch.nn.utils.rnn.pad_packed_sequence(
+    if self.use_packing:
+      gru_outputs, unpacked_len = torch.nn.utils.rnn.pad_packed_sequence(
                                         gru_outputs, batch_first=True)
-    our_embedding = self.output_layer(torch.mean(unpacked, dim=1))
+    our_embedding = self.output_layer(torch.mean(gru_outputs, dim=1))
     return our_embedding
