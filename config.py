6346276f980b78d6dd3e9cf4622ec7f84c6af0e1
Aneesh Pappu <apappu97@gmail.com>
2018-05-18 13:03:24 -0700

    updated learning rate from 1e-4 to 1e-3 bc was learning too slowly before

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class base_config(object):
         self.load_path = None
         # hyperparams
         self.random_seed=42
+        #original learning rate was 0.0001
         self.learning_rate=.001
         self.reg_weight = 0.1
         self.max_epochs=15
