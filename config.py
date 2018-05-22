87dba9fd9728556da2c2b56d5e7d5f183dd7afee
Rohun Saxena <rohuns@stanford.edu>
2018-05-21 23:03:07 -0700

    learnign rate reduced

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class base_config(object):
         self.load_path = None
         # hyperparams
         self.random_seed=42
-        self.learning_rate=.0001
+        self.learning_rate=.00005
         self.max_epochs=15
         self.batch_size=64
         self.n_hidden=250
