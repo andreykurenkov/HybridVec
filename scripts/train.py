bf95604dc9ffa3d7691c2c82b40a913d05deec4c
Harold Mei <harold@Tiger.modem>
2018-11-27 03:13:32 +1100

    Change to use config.json as configuration file

--- scripts/train.py
+++ scripts/train.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse
 import torch
 import torch._utils
+
 try:
     torch._utils._rebuild_tensor_v2
 except AttributeError:
@@ -18,12 +19,11 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.init as init
 import torchtext.vocab as vocab
-
 from torch.autograd import Variable
 
 from hybridvec.loader import *
-from hybridvec.config import train_config
-from hybridvec.models import Seq2seq, BaselineModel, EncoderRNN, DecoderRNN
+from hybridvec.config import *
+from hybridvec.models import *
 
 from tqdm import tqdm
 from time import time
@@ -38,45 +38,30 @@ config = train_config()
 TRAIN_FILE = 'data/glove/train_glove.%s.%sd.txt'%(config.vocab_source,config.vocab_dim)
 VAL_FILE = 'data/glove/val_glove.%s.%sd.txt'%(config.vocab_source, config.vocab_dim)
 
-def weights_init(m):
+def weights_init_xavier(m):
     """
     Initialize according to Xavier initialization or default initialization.
     """
-    if config.weight_init == 'xavier':
-        if type(m) in [nn.Linear]:
-            nn.init.xavier_normal(m.weight.data)
-        elif type(m) in [nn.LSTM, nn.RNN, nn.GRU]:
-            nn.init.xavier_normal(m.weight_hh_l0)
-            nn.init.xavier_normal(m.weight_ih_l0)
-
-def get_model_type():
-  """
-  Argument at command line for model type, either 'baseline' for baseline model 
-  or 's2s' for seq2seq model
-  """
-  parser = argparse.ArgumentParser()
-  parser.add_argument("model_type")
-  args = parser.parse_args()
-  return args.model_type 
+    if type(m) in [nn.Linear]:
+        nn.init.xavier_normal(m.weight.data)
+    elif type(m) in [nn.LSTM, nn.RNN, nn.GRU]:
+        nn.init.xavier_normal(m.weight_hh_l0)
+        nn.init.xavier_normal(m.weight_ih_l0)
 
 if __name__ == "__main__":
-    vocab = vocab.GloVe(name=config.vocab_source, 
-                        dim=config.vocab_dim)
+    vocab = vocab.GloVe(name=config.vocab_source, dim=config.vocab_dim)
     use_gpu = torch.cuda.is_available()
 
     print("Using GPU:", use_gpu)
     print ('vocab dim', config.vocab_dim)
 
     # continue from last training
-    parser = argparse.ArgumentParser()
-    parser.add_argument("run_name")
-    parser.add_argument("run_comment")
-    parser.add_argument("epoch")
-    args = parser.parse_args()
-    model_type, run_comment, epoch = args.run_name, args.run_comment, args.epoch
-    name = model_type + '-' + run_comment
-    config.load_path = "outputs/def2vec/checkpoints/{}/epoch_{}/model_weights.torch".format(name, epoch)
+    config = load_config()
+    writer, conf = init_experiment(config.__dict__)
+    save_config(config)
 
+    model_type = config.model_type
+    model_path = get_model_path(config)
     if model_type == 'baseline': 
         model = BaselineModel(vocab, 
                             config = config, 
@@ -90,37 +75,38 @@ if __name__ == "__main__":
         model = Seq2seq(encoder = encoder, 
                         decoder=decoder)
 
-    if config.load_path is None:
-        model.apply(weights_init)
+    if model_path is None or not os.path.exists(model_path):
+        model.apply(weights_init_xavier)
     else:
-        model.load_state_dict(torch.load(config.load_path))
-        
-    model.apply(weights_init)
+        model.load_state_dict(torch.load(model_path))
 
     if use_gpu:
         model = model.cuda()
 
-    train_loader = get_data_loader(TRAIN_FILE,
-                                   vocab,
-                                   config.input_method,
-                                   config.vocab_dim,
-                                   batch_size = config.batch_size,
-                                   num_workers = config.num_workers,
-                                   shuffle=config.shuffle,
-                                   vocab_size = config.vocab_size)
-    val_loader = get_data_loader(VAL_FILE,
-                                   vocab,
-                                   config.input_method,
-                                   config.vocab_dim,
-                                   batch_size = config.batch_size,
-                                   num_workers = config.num_workers,
-                                   shuffle=config.shuffle,
-                                   vocab_size = config.vocab_size)
-    optimizer = optim.Adam(model.parameters(),
-                           lr = config.learning_rate,
-                           weight_decay = config.weight_decay)
+    train_loader = get_data_loader(
+                            TRAIN_FILE,
+                            vocab,
+                            config.input_method,
+                            config.vocab_dim,
+                            batch_size = config.batch_size,
+                            num_workers = config.num_workers,
+                            shuffle=config.shuffle,
+                            vocab_size = config.vocab_size)
 
-    writer, conf = init_experiment(config.__dict__) #pytorch-monitor needs a dict
+    val_loader = get_data_loader(
+                            VAL_FILE,
+                            vocab,
+                            config.input_method,
+                            config.vocab_dim,
+                            batch_size = config.batch_size,
+                            num_workers = config.num_workers,
+                            shuffle=config.shuffle,
+                            vocab_size = config.vocab_size)
+
+    optimizer = optim.Adam(model.parameters(),
+                            lr = config.learning_rate,
+                            weight_decay = config.weight_decay)
+
     if DEBUG_LOG:
         monitor_module(model, writer)
 
@@ -130,9 +116,9 @@ if __name__ == "__main__":
     embed_labels = []
 
     for epoch in range(config.max_epochs):
-
         running_loss = 0.0
         start = time()
+
         print("Epoch", epoch)
         for i, data in enumerate(train_loader, 0):
             words, inputs, lengths, labels = data
@@ -145,11 +131,7 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             outputs = model(inputs, lengths)
             
-            loss_object, loss_val = model.calculate_loss(
-                                                    inputs, 
-                                                    outputs, 
-                                                    labels, 
-                                                    words)
+            loss_object, loss_val = model.calculate_loss(inputs, outputs, labels, words)
             loss_object.backward()
             optimizer.step()
 
@@ -203,18 +185,9 @@ if __name__ == "__main__":
                              (epoch + 1, i + 1, val_loss / len(val_loader)))
             # increase iteration
             total_iter += 1
-        
-        name = config.run_name + '-' + config.run_comment
-        out_dir = "outputs/def2vec/checkpoints/{}".format(config.run_name)
 
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-        
-        out_path = "outputs/def2vec/checkpoints/{}/epoch_{}".format(config.run_name, epoch + 1)
-        if not os.path.exists(out_path):
-            os.makedirs(out_path)
-        
-        torch.save(model.state_dict(), out_path + "/" + config.save_path)
+        config.load_epoch = epoch + 1
+        torch.save(model.state_dict(), get_model_path(config))
 
     writer.export_scalars_to_json("./all_scalars.json")
     writer.close()
